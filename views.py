5e8ab68b6ba5cfb01f8778c17d761233bfc45114
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-03-17 20:08:31 -0300

    feat: adicionar a conversão de inteiro para hexadecimal

--- views.py
+++ views.py
@@ -9,7 +9,6 @@ def int_to_binary(n : int):
         n = result
 
     binary_digits.append(1)
-    
 
     binary_number = 0
     for char in binary_digits[::-1]:
@@ -21,6 +20,24 @@ def int_to_binary(n : int):
     
     return binary_number
 
+def int_to_hexa(n : int):
+    values = {x : y for x, y in zip(range(10, 16), ['A', 'B', 'C', 'D', 'E', 'F'])}
+    hexa_digits = []
+    result = 16
+    while result >= 16:
+        result = n // 16
+        rest = n % 16
+        hexa_digits.append(rest)
+        n = result
 
+    hexa_digits.append(result)
 
+    hexa_number = ''
+    for char in hexa_digits[::-1]:
+        if char <= 9:
+            hexa_number += str(char)
+        else:
+            hexa_number += values[char]
+
+    return hexa_number
 
